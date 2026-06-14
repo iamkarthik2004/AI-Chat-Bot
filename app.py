76046930ee542ee58d6aa5b60ae143b33581aa51
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2026-06-14 17:31:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+st.title("🤖 Smart AI Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+user_input = st.chat_input("Type your message...")
+
+if user_input:
+    st.session_state.messages.append(
+        {"role": "user", "content": user_input}
+    )
+
+    with st.chat_message("user"):
+        st.write(user_input)
+
+    response = model.generate_content(user_input)
+
+    bot_reply = response.text
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": bot_reply}
+    )
+
+    with st.chat_message("assistant"):
+        st.write(bot_reply)
